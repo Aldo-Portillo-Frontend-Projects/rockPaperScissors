98ad9cd7f65690e8677c5491731af3eae4d0a416
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-07-24 23:44:29 -0500

    Create playRound function

--- main.js
+++ main.js
@@ -4,4 +4,35 @@ let getComputerChoice = () => {
     return arr[Math.floor(Math.random() * 3)]
   }
 
-  console.log(getComputerChoice());
+  //console.log(getComputerChoice());
+
+  let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection){
+        return 'There is a Tie!';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock';
+        } else {
+            return 'You Win! Rock beats scissors';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'You Lose! Scissors beats paper';
+        } else {
+            return 'You Win! Paper beats rock';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You Lose! Rock beats scissors';
+        } else {
+            return 'You Win! Scissors beats paper'
+        }
+    } else {
+        return 'invalid input';
+    }
+  }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
